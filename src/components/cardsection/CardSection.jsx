5f80d8f7f964b51e317432ee19dd07145857b597
Sakib <sahadothosain6619@gmail.com>
2024-04-24 12:22:36 +0600

    added galry section with animation

--- src/components/cardsection/CardSection.jsx
+++ src/components/cardsection/CardSection.jsx
@@ -6,20 +6,105 @@ const CardSection = () => {
     <section>
       <div className="container">
         <div className="grid-card">
-          <div className="card-one">
+          <div className="card card-one">
             <img className="img-fluid" src={card1} alt="" />
+            <div className="card-hover">
+              <div className="row justify-content-center align-items-center">
+                <div className="col">
+                  <h6>AI generated art.</h6>
+                  <p>$200 USD in 7 days</p>
+                </div>
+                <div className="col">
+                  <a
+                    className="btn-five p-2 wow fadeInUp"
+                    data-wow-delay="0.6s"
+                    href="/register"
+                  >
+                    Visit more
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card-tow">
+          <div className="card card-tow">
             <img className="img-fluid" src={card1} alt="" />
+            <div className="card-hover">
+              <div className="row justify-content-center align-items-center">
+                <div className="col">
+                  <h6>AI generated art.</h6>
+                  <p>$200 USD in 7 days</p>
+                </div>
+                <div className="col">
+                  <a
+                    className="btn-five p-2 wow fadeInUp"
+                    data-wow-delay="0.6s"
+                    href="/register"
+                  >
+                    Visit more
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card-there">
+          <div className="card card-there">
             <img className="img-fluid" src={card1} alt="" />
+            <div className="card-hover">
+              <div className="row justify-content-center align-items-center">
+                <div className="col">
+                  <h6>AI generated art.</h6>
+                  <p>$200 USD in 7 days</p>
+                </div>
+                <div className="col">
+                  <a
+                    className="btn-five p-2 wow fadeInUp"
+                    data-wow-delay="0.6s"
+                    href="/register"
+                  >
+                    Visit more
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card-for">
+          <div className="card card-for">
             <img className="img-fluid" src={card1} alt="" />
+            <div className="card-hover">
+              <div className="row justify-content-center align-items-center">
+                <div className="col">
+                  <h6>AI generated art.</h6>
+                  <p>$200 USD in 7 days</p>
+                </div>
+                <div className="col">
+                  <a
+                    className="btn-five p-2 wow fadeInUp"
+                    data-wow-delay="0.6s"
+                    href="/register"
+                  >
+                    Visit more
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card-five">
+          <div className="card card-five">
             <img className="img-fluid" src={card1} alt="" />
+            <div className="card-hover">
+              <div className="row justify-content-center align-items-center">
+                <div className="col">
+                  <h6>AI generated art.</h6>
+                  <p>$200 USD in 7 days</p>
+                </div>
+                <div className="col">
+                  <a
+                    className="btn-five p-2 wow fadeInUp"
+                    data-wow-delay="0.6s"
+                    href="/register"
+                  >
+                    Visit more
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
