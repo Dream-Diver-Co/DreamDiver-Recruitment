0529813503862a97a3c526c845112e3f36050b8e
Sakib <sahadothosain6619@gmail.com>
2024-04-24 12:43:22 +0600

    solve problem

--- src/components/cardsection/CardSection.jsx
+++ src/components/cardsection/CardSection.jsx
@@ -2,110 +2,65 @@ import React from "react";
 import "./cardSection.css";
 import card1 from "../../assets/images/1817900-medicare.jpg";
 const CardSection = () => {
+  const makeYourDreamCard = [
+    {
+      title: "AI generated art.",
+      price: "$200 USD in 7 days",
+      image: card1,
+      link: "/",
+    },
+    {
+      title: "AI generated art.",
+      price: "$200 USD in 7 days",
+      image: card1,
+      link: "/",
+    },
+    {
+      title: "AI generated art.",
+      price: "$200 USD in 7 days",
+      image: card1,
+      link: "/",
+    },
+    {
+      title: "AI generated art.",
+      price: "$200 USD in 7 days",
+      image: card1,
+      link: "/",
+    },
+    {
+      title: "AI generated art.",
+      price: "$200 USD in 7 days",
+      image: card1,
+      link: "/",
+    },
+  ];
   return (
     <section>
       <div className="container">
+        <h1 style={{ color: "#005025" }}>Make your dreams a reality</h1>
         <div className="grid-card">
-          <div className="card card-one">
-            <img className="img-fluid" src={card1} alt="" />
-            <div className="card-hover">
-              <div className="row justify-content-center align-items-center">
-                <div className="col">
-                  <h6>AI generated art.</h6>
-                  <p>$200 USD in 7 days</p>
-                </div>
-                <div className="col">
-                  <a
-                    className="btn-five p-2 wow fadeInUp"
-                    data-wow-delay="0.6s"
-                    href="/register"
-                  >
-                    Visit more
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card card-tow">
-            <img className="img-fluid" src={card1} alt="" />
-            <div className="card-hover">
-              <div className="row justify-content-center align-items-center">
-                <div className="col">
-                  <h6>AI generated art.</h6>
-                  <p>$200 USD in 7 days</p>
-                </div>
-                <div className="col">
-                  <a
-                    className="btn-five p-2 wow fadeInUp"
-                    data-wow-delay="0.6s"
-                    href="/register"
-                  >
-                    Visit more
-                  </a>
+          {makeYourDreamCard.map((data, index) => (
+            <div key={index} className={`card card${index}`}>
+              <img className="img-fluid" src={data.image} alt="" />
+              <div className="card-hover">
+                <div className="row justify-content-center align-items-center">
+                  <div className="col">
+                    <h6>{data.title}</h6>
+                    <p>{data.price}</p>
+                  </div>
+                  <div className="col">
+                    <a
+                      className="btn-five p-2 wow fadeInUp"
+                      data-wow-delay="0.6s"
+                      href={data.link}
+                    >
+                      Visit more
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="card card-there">
-            <img className="img-fluid" src={card1} alt="" />
-            <div className="card-hover">
-              <div className="row justify-content-center align-items-center">
-                <div className="col">
-                  <h6>AI generated art.</h6>
-                  <p>$200 USD in 7 days</p>
-                </div>
-                <div className="col">
-                  <a
-                    className="btn-five p-2 wow fadeInUp"
-                    data-wow-delay="0.6s"
-                    href="/register"
-                  >
-                    Visit more
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card card-for">
-            <img className="img-fluid" src={card1} alt="" />
-            <div className="card-hover">
-              <div className="row justify-content-center align-items-center">
-                <div className="col">
-                  <h6>AI generated art.</h6>
-                  <p>$200 USD in 7 days</p>
-                </div>
-                <div className="col">
-                  <a
-                    className="btn-five p-2 wow fadeInUp"
-                    data-wow-delay="0.6s"
-                    href="/register"
-                  >
-                    Visit more
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card card-five">
-            <img className="img-fluid" src={card1} alt="" />
-            <div className="card-hover">
-              <div className="row justify-content-center align-items-center">
-                <div className="col">
-                  <h6>AI generated art.</h6>
-                  <p>$200 USD in 7 days</p>
-                </div>
-                <div className="col">
-                  <a
-                    className="btn-five p-2 wow fadeInUp"
-                    data-wow-delay="0.6s"
-                    href="/register"
-                  >
-                    Visit more
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
