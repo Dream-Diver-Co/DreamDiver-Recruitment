cef97cd08b427379d5a1bde15584f906f6659277
Sakib <sahadothosain6619@gmail.com>
2024-04-27 13:13:39 +0600

    added responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,91 +10,108 @@ import { MdOutlineLanguage } from "react-icons/md";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__top">
-        <div className="footer__top--left">
-          <img className="img-fluid" src={logo} alt="logo" />
-          <p>
-            <MdOutlineLanguage /> &nbsp; US (International)/Engpsh
-          </p>
-          <p>
-            <IoIosHelpCircleOutline /> &nbsp; Help & Support
-          </p>
-        </div>
-        <div className="footer__top--right">
-          <div className="footer__top--right-one">
-            <h4>Freelancer</h4>
-            <p>Categories</p>
-            <p>Categories</p>
-            <p>Projects</p>
-            <p>Contests</p>
-            <p>Freelancers</p>
-            <p>Enterprise</p>
-            <p>Preferred Freelancer</p>
-            <p>Program</p>
-            <p>Project Management</p>
-            <p>Local Jobs</p>
-            <p>Showcase</p>
-            <p>API for Developers</p>
-            <p>Get Verified</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <div className="footer__top--left">
+              <img className="img-fluid" src={logo} alt="logo" />
+              <p>
+                <MdOutlineLanguage /> &nbsp; US (International)/Engpsh
+              </p>
+              <p>
+                <IoIosHelpCircleOutline /> &nbsp; Help & Support
+              </p>
+            </div>
           </div>
-          <div className="footer__top--right-two">
-            <h4>About</h4>
-            <p>About Us</p>
-            <p>How it Works</p>
-            <p>Security</p>
-            <p>Investor</p>
-            <p>Sitemap</p>
-            <p>Quotes</p>
-            <p>News</p>
+          <div className="col-10">
+            <div className="row">
+              <div className="footer__top--right">
+                <div className="col col-md-3">
+                  <div className="footer__top--right-one">
+                    <h4>Freelancer</h4>
+                    <p>Categories</p>
+                    <p>Categories</p>
+                    <p>Projects</p>
+                    <p>Contests</p>
+                    <p>Freelancers</p>
+                    <p>Enterprise</p>
+                    <p>Preferred Freelancer</p>
+                    <p>Program</p>
+                    <p>Project Management</p>
+                    <p>Local Jobs</p>
+                    <p>Showcase</p>
+                    <p>API for Developers</p>
+                    <p>Get Verified</p>
+                  </div>
+                </div>
+                <div className="col col-md-3">
+                  <div className="footer__top--right-two">
+                    <h4>About</h4>
+                    <p>About Us</p>
+                    <p>How it Works</p>
+                    <p>Security</p>
+                    <p>Investor</p>
+                    <p>Sitemap</p>
+                    <p>Quotes</p>
+                    <p>News</p>
+                  </div>
+                </div>
+                <div className="col col-md-3">
+                  <div className="footer__top--right-three">
+                    <h4>Terms</h4>
+                    <p>Privacy Popcy</p>
+                    <p>Terms and Conditions</p>
+                    <p>Copyright Popcy</p>
+                    <p>Code of Conduct</p>
+                    <p>Fees and Charges</p>
+                  </div>
+                </div>
+                <div className="col col-md-3">
+                  <div className="footer__top--right-four">
+                    <h4>Apps</h4>
+                    <img
+                      src="https://damassets.autodesk.net/content/dam/autodesk/www/products/autocad-mobile/fy18/general/appstore-google.png"
+                      alt="apple"
+                    />
+                    <br />
+                    <img
+                      src="https://damassets.autodesk.net/content/dam/autodesk/www/products/autocad-mobile/fy18/general/appstore-itunes.png"
+                      alt="apple"
+                    />
+                    <p>
+                      <FaFacebook />
+                      <AiFillTwitterCircle />
+                      <RiYoutubeFill />
+                      <FaInstagram />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="footer__top--right-three">
-            <h4>Terms</h4>
-            <p>Privacy Popcy</p>
-            <p>Terms and Conditions</p>
-            <p>Copyright Popcy</p>
-            <p>Code of Conduct</p>
-            <p>Fees and Charges</p>
+          <div className="footer__top"></div>
+          <hr />
+          <div id="dream-diver" className=" w-100 text-white py-2">
+            <div className="container">
+              <p className="text-end mb-0">
+                Developed
+                <a
+                  href="https://www.linkedin.com/in/sakib6619/"
+                  className="text-light"
+                >
+                  {" "}
+                  by
+                </a>{" "}
+                <a
+                  href="https://dreamdiver.nl/"
+                  rel="noreferrer"
+                  className="text-dreamDiver"
+                >
+                  DREAM DIVER
+                </a>
+              </p>
+            </div>
           </div>
-          <div className="footer__top--right-four">
-            <h4>Apps</h4>
-            <img
-              src="https://damassets.autodesk.net/content/dam/autodesk/www/products/autocad-mobile/fy18/general/appstore-google.png"
-              alt="apple"
-            />
-            <br />
-            <img
-              src="https://damassets.autodesk.net/content/dam/autodesk/www/products/autocad-mobile/fy18/general/appstore-itunes.png"
-              alt="apple"
-            />
-            <p>
-              <FaFacebook />
-              <AiFillTwitterCircle />
-              <RiYoutubeFill />
-              <FaInstagram />
-            </p>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div id="dream-diver" className="bg-dark w-100 text-white py-2">
-        <div className="container">
-          <p className="text-end mb-0">
-            Developed
-            <a
-              href="https://www.linkedin.com/in/sakib6619/"
-              className="text-light"
-            >
-              {" "}
-              by
-            </a>{" "}
-            <a
-              href="https://dreamdiver.nl/"
-              rel="noreferrer"
-              className="text-dreamDiver"
-            >
-              DREAM DIVER
-            </a>
-          </p>
         </div>
       </div>
     </div>
