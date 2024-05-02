1b18abf1f956fc431aceaae55d7dca2575d89f6e
Sakib <sahadothosain6619@gmail.com>
2024-05-02 15:39:14 +0600

    added toogle mega menu

--- src/components/header/Manu0.jsx
+++ src/components/header/Manu0.jsx
@@ -6,67 +6,218 @@ import { GrTechnology } from "react-icons/gr";
 const Manu0 = () => {
   return (
     <div className="card">
-      <div className=" bg-dark ">
+      <div className="bg-dark">
         <div className="row">
           <div className="mega-menu-bg col-md-4">
-            <ul>
+            <ul className="mega-menu-category">
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <AiFillCode className="m-1 fs-5" />
-                <p className="m-0">Soft Jobs</p>
+                <a className="text-white mega-menu-active" href="/">
+                  Soft Jobs
+                </a>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <FaBusinessTime className="m-1 fs-5" />
-                <p className="m-0">Hard Jobs</p>
+                <a className="text-white " href="/">
+                  Hard Jobs
+                </a>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <GrTechnology className="m-1 fs-5" />
-                <p className="m-0">Technical Jobs</p>
+                <a className="text-white " href="/">
+                  Technical Jobs
+                </a>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <FaLaptopHouse className="m-1 fs-5" />
-                <p className="m-0">Houseful Jobs</p>
+                <a className="text-white " href="/">
+                  House hold Jobs
+                </a>
               </li>
             </ul>
           </div>
-          <div className="col-md-4">
-            <ul>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <AiFillCode className="m-1 fs-5" />
-                <p className="m-0">Soft Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <FaBusinessTime className="m-1 fs-5" />
-                <p className="m-0">Hard Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <GrTechnology className="m-1 fs-5" />
-                <p className="m-0">Technical Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <FaLaptopHouse className="m-1 fs-5" />
-                <p className="m-0">Houseful Jobs</p>
-              </li>
-            </ul>
+          <div className="col-md-8 sub-mega-menu mega-show">
+            <div className="row">
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <AiFillCode className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Product IT Support
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaBusinessTime className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Sales Employee
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      IT Support
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Sales Employee
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Administrative assistant
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Customer Service
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="col-md-4">
-            <ul>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <AiFillCode className="m-1 fs-5" />
-                <p className="m-0">Soft Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <FaBusinessTime className="m-1 fs-5" />
-                <p className="m-0">Hard Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <GrTechnology className="m-1 fs-5" />
-                <p className="m-0">Technical Jobs</p>
-              </li>
-              <li className="d-flex justify-items-center m-1 p-1 text-center">
-                <FaLaptopHouse className="m-1 fs-5" />
-                <p className="m-0">Houseful Jobs</p>
-              </li>
-            </ul>
+          {/* heard job */}
+          <div className="col-md-8 mega-hidden">
+            <div className="row">
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <AiFillCode className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Waiter / waitress
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaBusinessTime className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      All-round warehouse employee
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      All-round store employee
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      All-round warehouse employee
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* technical hold jobs */}
+          <div className="col-md-8 mega-hidden">
+            <div className="row">
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <AiFillCode className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Cashier
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaBusinessTime className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Order picker
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      IT Support
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Sales Employee
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* house hold jobs */}
+          <div className="col-md-8 mega-hidden">
+            <div className="row">
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <AiFillCode className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Cook
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaBusinessTime className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Kitchen porter
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Kitchen Aid
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Cleaning work
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <ul>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <GrTechnology className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Cashier
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Cleaner
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Housekeeping
+                    </a>
+                  </li>
+                  <li className="d-flex justify-items-center m-1 p-1 text-left">
+                    <FaLaptopHouse className="m-1 fs-5" />
+                    <a className="text-white" href="/">
+                      Host/Hostess
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
