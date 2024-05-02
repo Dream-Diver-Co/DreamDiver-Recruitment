d6223e1bf7c57d070bf1ff67fe7b8ae5d2c64015
Sakib <sahadothosain6619@gmail.com>
2024-05-02 16:17:28 +0600

    added mega hover

--- src/components/header/Manu0.jsx
+++ src/components/header/Manu0.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./manu.css";
 import { AiFillCode } from "react-icons/ai";
 import { FaBusinessTime, FaLaptopHouse } from "react-icons/fa";
 import { GrTechnology } from "react-icons/gr";
+import { Link } from "react-router-dom";
+import SoftJobs from "./SoftJobs";
+import HeardJobs from "./HeardJobs";
+import TecnicalJobs from "./TecnicalJobs";
+import HouseHoldjobs from "./HouseHoldjobs";
 const Manu0 = () => {
+  const [hoverData, setHoverData] = useState(null);
+
   return (
     <div className="card">
       <div className="bg-dark">
@@ -12,213 +19,57 @@ const Manu0 = () => {
             <ul className="mega-menu-category">
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <AiFillCode className="m-1 fs-5" />
-                <a className="text-white mega-menu-active" href="/">
+                <Link
+                  className="text-white mega-menu-active"
+                  to="/"
+                  onMouseEnter={() => setHoverData(0)}
+                >
                   Soft Jobs
-                </a>
+                </Link>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <FaBusinessTime className="m-1 fs-5" />
-                <a className="text-white " href="/">
+                <a
+                  className="text-white "
+                  href="/"
+                  onMouseEnter={() => setHoverData(1)}
+                >
                   Hard Jobs
                 </a>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <GrTechnology className="m-1 fs-5" />
-                <a className="text-white " href="/">
+                <a
+                  className="text-white "
+                  href="/"
+                  onMouseEnter={() => setHoverData(2)}
+                >
                   Technical Jobs
                 </a>
               </li>
               <li className="d-flex justify-items-center m-1 p-1 text-center">
                 <FaLaptopHouse className="m-1 fs-5" />
-                <a className="text-white " href="/">
+                <a
+                  className="text-white "
+                  href="/"
+                  onMouseEnter={() => setHoverData(3)}
+                >
                   House hold Jobs
                 </a>
               </li>
             </ul>
           </div>
-          <div className="col-md-8 sub-mega-menu mega-show">
-            <div className="row">
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <AiFillCode className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Product IT Support
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaBusinessTime className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Sales Employee
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      IT Support
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Sales Employee
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Administrative assistant
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Customer Service
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          {/* heard job */}
-          <div className="col-md-8 mega-hidden">
-            <div className="row">
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <AiFillCode className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Waiter / waitress
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaBusinessTime className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      All-round warehouse employee
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      All-round store employee
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      All-round warehouse employee
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          {/* technical hold jobs */}
-          <div className="col-md-8 mega-hidden">
-            <div className="row">
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <AiFillCode className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Cashier
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaBusinessTime className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Order picker
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      IT Support
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Sales Employee
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          {/* house hold jobs */}
-          <div className="col-md-8 mega-hidden">
-            <div className="row">
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <AiFillCode className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Cook
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaBusinessTime className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Kitchen porter
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Kitchen Aid
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Cleaning work
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6">
-                <ul>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <GrTechnology className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Cashier
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Cleaner
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Housekeeping
-                    </a>
-                  </li>
-                  <li className="d-flex justify-items-center m-1 p-1 text-left">
-                    <FaLaptopHouse className="m-1 fs-5" />
-                    <a className="text-white" href="/">
-                      Host/Hostess
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          {hoverData === 0 ? (
+            <SoftJobs />
+          ) : hoverData === 1 ? (
+            <HeardJobs />
+          ) : hoverData === 2 ? (
+            <TecnicalJobs />
+          ) : hoverData === 3 ? (
+            <HouseHoldjobs />
+          ) : (
+            <SoftJobs />
+          )}
         </div>
       </div>
     </div>
---
+++ src/components/header/TecnicalJobs.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { AiFillCode } from "react-icons/ai";
+import { FaBusinessTime, FaLaptopHouse } from "react-icons/fa";
+import { GrTechnology } from "react-icons/gr";
+
+const TecnicalJobs = () => {
+  return (
+    <div className="col-md-8 ">
+      <div className="row">
+        <div className="col-md-6">
+          <ul>
+            <li className="d-flex justify-items-center m-1 p-1 text-left">
+              <AiFillCode className="m-1 fs-5" />
+              <a className="text-white" href="/">
+                Cashier
+              </a>
+            </li>
+            <li className="d-flex justify-items-center m-1 p-1 text-left">
+              <FaBusinessTime className="m-1 fs-5" />
+              <a className="text-white" href="/">
+                Order picker
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <ul>
+            <li className="d-flex justify-items-center m-1 p-1 text-left">
+              <GrTechnology className="m-1 fs-5" />
+              <a className="text-white" href="/">
+                IT Support
+              </a>
+            </li>
+            <li className="d-flex justify-items-center m-1 p-1 text-left">
+              <FaLaptopHouse className="m-1 fs-5" />
+              <a className="text-white" href="/">
+                Sales Employee
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TecnicalJobs;
