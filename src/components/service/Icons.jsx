ba56389c84de16a45f85109dfa55d0f353d70211
Shahadot Hossain <79484476+sakibbdcse@users.noreply.github.com>
2024-05-07 10:39:21 +0600

    Master (#60)
    * test
    
    * added test2
    
    * sfdh
    
    * added
    
    ---------
    
    Co-authored-by: Dream Diver <dreamdiver2017@gmail.com>

--- src/components/service/Icons.jsx
+++ src/components/service/Icons.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./icons.css";
+import { Link } from "react-router-dom";
 const Icons = ({ src, title }) => {
   return (
     <div className="icons">
       <img src={src} alt="icons" />
-      <h6>{title}</h6>
+      <Link className="nav-link" to="/">
+        <h6>{title}</h6>
+      </Link>
     </div>
   );
 };
