75fdf6680bc53df54bbf92733e7e03f6cff08688
Sakib <sahadothosain6619@gmail.com>
2024-05-15 15:47:49 +0600

    solve the rechatcha issue

--- src/components/employForm/EmployForm.jsx
+++ src/components/employForm/EmployForm.jsx
@@ -218,7 +218,8 @@ const EmployForm = () => {
                   </div>
                 </div>
                 <ReCAPTCHA
-                  sitekey="6LfDstkpAAAAAOJKaEQw9sgvSySi24oqIx7ig-0T"
+                  className="d-flex justify-content-center  m-2"
+                  sitekey="6LftLt0pAAAAAFlhGGN6dSLeey_a2ovjT-freAzU"
                   onChange={(value) => setVerified(value)}
                 />
                 <input
